1c859b19cc37f914eb140ace1d0d60e7514adc19
Ruben <ruben.de.facq@redpencil.io>
2024-09-17 13:54:46 +0200

    initial restructure of service sync for new submission model

--- app.js
+++ app.js
@@ -22,12 +22,13 @@ async function startSync() {
   // search in any graph (= in any `http://mu.semte.ch/graphs/organizations/` graph)
   const queryString = `
   PREFIX dct: <http://purl.org/dc/terms/>
-  PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
+  PREFIX dbpedia: <http://dbpedia.org/ontology/>
+  PREFIX omgeving: <https://data.vlaanderen.be/ns/omgevingsvergunning#>
   SELECT ?created 
   {
     GRAPH ?g {
-      ?uri a ext:Aanvraag ;
-           dct:created ?created .
+      ?uri a dbpedia:Case ;
+           omgeving:ingangsdatum ?created .
     }
   } ORDER BY DESC(?created) LIMIT 1
   `;
@@ -40,53 +41,139 @@ async function startSync() {
     date = "2024-01-01T00:00:00Z";
   }
 
-  // org is refered to "ext:sessionGroup" in GN?
-  const queryAanvragen = `
+  // takes triples of Case and one-deep relationships.
+  const queryCases = `
     PREFIX dct: <http://purl.org/dc/terms/>
     PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
     PREFIX mu:   <http://mu.semte.ch/vocabularies/core/>
-    SELECT ?uri ?p ?o ?orgId 
-    {
+    PREFIX dbpedia: <http://dbpedia.org/ontology/>
+    PREFIX omgeving: <https://data.vlaanderen.be/ns/omgevingsvergunning#>
+    SELECT ?uri {
       GRAPH ?g {
-       select ?uri {
-        ?uri a ext:Aanvraag ;
-            dct:created ?created .
+       select ?uri ?created {
+        ?uri a dbpedia:Case ;
+            omgeving:ingangsdatum ?created .
         FILTER ( ?created > ${sparqlEscapeDateTime(date)})    
-      }  LIMIT 20
-        ?uri ?p ?o .
-        ?uri ext:Org ?org .
-        ?org mu:uuid ?orgId .
-      }
-    } ORDER BY DESC(?created)
+      } ORDER BY DESC(?created) LIMIT 10 
+    }
   `;
   // do this query to the external sparql endpoint (?)
   const connectionOptions = {
     sparqlEndpoint: "http://the.custom.endpoint/sparql",
     mayRetry: true,
   };
-  result = await querySudo(queryAanvragen, {}, connectionOptions);
+  result = await querySudo(queryCases, {}, connectionOptions);
   bindings = result.results.bindings;
-  if (bindings.length > 0) {
-    // new aanvragen, add to database
-    const triplesToAdd = collapsArrayOfObjects(
-      bindings.map((b) => ({
-        org: b.org.value,
-        triple: `${b.uri.value} ${b.p.value} ${b.o.value}`,
-      })), "org", "triple", "triples"
-    );
-    const queryAddAanvragen = `
-    INSERT DATA {
-      ${triplesToAdd.map(({org, triples}) => {
-        `
-        GRAPH <${PREFIX_ORGANIZATION_GRAPH}${org}> {
-        ${triples.join(" .\n")}
-      }`
-      })}
-      
+  if (bindings.length === 0) {
+    return; // no new cases
+  }  
+  const urisCases = bindings.map(b => b.uri.value);
+  // query all the triples that should be transfered.
+  // omgeving:zaakhandeling is handled separately to only copy to the org (municipality) it is part of
+  const queryInfo = `
+    PREFIX dct: <http://purl.org/dc/terms/>
+    PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
+    PREFIX mu:   <http://mu.semte.ch/vocabularies/core/>
+    PREFIX dbpedia: <http://dbpedia.org/ontology/>
+    PREFIX omgeving: <https://data.vlaanderen.be/ns/omgevingsvergunning#>
+    SELECT DISTINCT ?orgId ?s ?p ?o {
+      GRAPH ?g {
+       ?uri omgeving:zaakhandeling ?submission . 
+       ?submission omgeving:Rechtshandeling.verantwoordelijke/mu:uuid ?orgId .
+       {
+        ?uri ?p ?o .
+        ?s ?p ?o .
+        FILTER( p != omgeving:zaakhandeling )
+       }
+       UNION {
+        ?uri dct:subject ?s .
+        ?s ?p ?o . 
+       } UNION {
+        ?submission ?p ?s .
+        ?s ?p ?o . 
+       }
+        UNION {
+         ?uri dct:subject/omgeving:locatie ?s .
+        ?s ?p ?o . 
+       }
+        UNION {
+         ?uri dct:subject/omgeving:Activiteit.tijdsbestek ?s .
+        ?s ?p ?o . 
+       } UNION {
+         ?submission omgeving:aanvrager ?s .
+        ?s ?p ?o . 
+       } UNION {
+        ?submission omgeving:aanvrager ?s .
+        ?s ?p ?o . 
+       }
+       VALUES ?uri { ${urisCases.map(uri => sparqlEscapeUri(uri)).join(" ")} }
     }
   `;
-    await queryUpdate(queryAddAanvragen);
+  // new aanvragen, add to database
+  // todo: Will these include the neccesary mu:uuid's ?
+  result = await querySudo(queryInfo, {}, connectionOptions);
+  bindings = result.results.bindings;
+  if (bindings.length === 0) {
+    return; // no info?
   }
+  const triplesToAdd = collapsArrayOfObjects(
+    bindings.map((b) => ({
+      orgId: b.orgId.value,
+      triple: `${b.s.value} ${b.p.value} ${b.o.value}`,
+    })), "orgId", "triple", "triples"
+  );
+
+  const queryAddAanvragen = `
+  INSERT DATA {
+    ${triplesToAdd.map(({orgId, triples}) => {
+      `
+      GRAPH <${PREFIX_ORGANIZATION_GRAPH}${orgId}> {
+      ${triples.join(" .\n")}
+    }`
+    })}
+  }
+`;
+  await updateSudo(queryAddAanvragen);
+
+  // add the link between cases and submission where allowed.
+  const querySubmissionOfCase = `
+    PREFIX dct: <http://purl.org/dc/terms/>
+    PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
+    PREFIX mu:   <http://mu.semte.ch/vocabularies/core/>
+    PREFIX dbpedia: <http://dbpedia.org/ontology/>
+    PREFIX omgeving: <https://data.vlaanderen.be/ns/omgevingsvergunning#>
+    SELECT DISTINCT ?orgId ?uri ?submission {
+      GRAPH ?g {
+      ?uri omgeving:zaakhandeling ?submission . 
+      ?submission omgeving:Rechtshandeling.verantwoordelijke/mu:uuid ?orgId .
+      VALUES ?uri { ${urisCases.map(uri => sparqlEscapeUri(uri)).join(" ")} }
+    }
+  `;
+  result = await querySudo(querySubmissionOfCase, {}, connectionOptions);
+  bindings = result.results.bindings;
+  if (bindings.length === 0) {
+    return; // no info?
+  }
+
+  const submissionsToAdd = collapsArrayOfObjects(
+    bindings.map((b) => ({
+      orgId: b.orgId.value,
+      triple: `${b.uri.value} omgeving:zaakhandeling ${b.submission.value}`,
+    })), "orgId", "triple", "triples"
+  );
+
+  const submissionsToAddQuery = `
+  INSERT DATA {
+    ${submissionsToAdd.map(({orgId, triples}) => {
+      `
+      GRAPH <${PREFIX_ORGANIZATION_GRAPH}${orgId}> {
+      ${triples.join(" .\n")}
+    }`
+    })}
+  }
+`;
+  await updateSudo(submissionsToAddQuery);
+  
 }
 
 app.use(errorHandler);
